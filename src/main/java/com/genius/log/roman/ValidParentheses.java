d83bdc5f473472d4819afd68bfb3247db19717ed
Yu Shuai Qi <shuaiqi@ifastfinancial.com.cn>
2019-12-30 16:17:22 +0800

    parentheses

---
+++ src/main/java/com/genius/log/roman/ValidParentheses.java
@@ -0,0 +1,22 @@
+package com.genius.log.roman;
+
+/**
+ * @类名 ValidParentheses
+ * @描述 ValidParentheses
+ * @作者 shuaiqi
+ * @创建日期 2019/12/26 22:13
+ * @版本号 1.0
+ * @参考地址
+ **/
+public class ValidParentheses {
+
+	public static void main(String[] args) {
+
+	}
+
+	public boolean isValid() {
+
+		return false;
+	}
+
+}
